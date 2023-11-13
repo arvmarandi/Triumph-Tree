d3734c7705cf3a8f715bd38df1101928451063d6
arvmarandi <arv.marandi@gmail.com>
2023-11-13 09:37:21 -0800

    Navigate between activities

--- app/src/main/java/com/example/triumphtree/NewGoal.kt
+++ app/src/main/java/com/example/triumphtree/NewGoal.kt
@@ -1,11 +1,19 @@
 package com.example.triumphtree
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NewGoal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_goal)
+
+        val button = findViewById<Button>(R.id.return_home)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
