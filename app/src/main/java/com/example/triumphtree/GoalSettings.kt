d411555ce2d45473bb7dd504cda35e58249da90f
arvmarandi <arv.marandi@gmail.com>
2023-11-14 18:36:25 -0800

    Functionality for reminder switch... i.e., when the switch is on, frequency spinner is visibile, but when the switch is off, frequency spinner is gone.

--- app/src/main/java/com/example/triumphtree/GoalSettings.kt
+++ app/src/main/java/com/example/triumphtree/GoalSettings.kt
@@ -1,11 +1,44 @@
 package com.example.triumphtree
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.CompoundButton
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
+
 
 class GoalSettings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_goal_settings)
+
+        val reminderSwitch = findViewById<SwitchCompat>(R.id.on_off_switch)
+        val frequencySpinner = findViewById<Spinner>(R.id.notification_frequency)
+        val adapter = ArrayAdapter.createFromResource(
+            this, R.array.frequency_array, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        frequencySpinner.adapter = adapter
+
+        frequencySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val item = parent?.getItemAtPosition(position) as String
+                Toast.makeText(this@GoalSettings, item, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        reminderSwitch.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { _, isChecked -> // Show/hide the spinner based on the switch state
+            if (isChecked) {
+                frequencySpinner.visibility = View.VISIBLE
+            } else {
+                frequencySpinner.visibility = View.GONE
+            }
+        })
+
     }
 }
