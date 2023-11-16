86ffafcdb1c300221fbf38ec54400c0ae3357a0c
finndugan <finnpdugan@gmail.com>
2023-11-15 19:33:21 -0800

    Added app bar to main activity

--- app/src/main/java/com/example/triumphtree/MainActivity.kt
+++ app/src/main/java/com/example/triumphtree/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.triumphtree
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.app_bar)
+        setSupportActionBar(toolbar)
 
         val button = findViewById<Button>(R.id.new_goal)
         button.setOnClickListener {
@@ -22,5 +27,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.app_bar, menu)
+        return true
     }
 }
