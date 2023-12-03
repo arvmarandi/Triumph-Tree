90fda3725cc617612f749ec5b24787d059b2a06a
arvmarandi <arv.marandi@gmail.com>
2023-12-03 15:53:14 -0800

    Added a rudimentary navigation drawer with placeholder items. Changed the base theme to include an action bar.

--- app/src/main/java/com/example/triumphtree/MainActivity.kt
+++ app/src/main/java/com/example/triumphtree/MainActivity.kt
@@ -4,16 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawerID: DrawerLayout
+    lateinit var toggle: ActionBarDrawerToggle
+    lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar: Toolbar = findViewById(R.id.app_bar)
-        setSupportActionBar(toolbar)
+        drawerID = findViewById(R.id.my_drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawerID, R.string.open, R.string.close)
+
+        drawerID.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView = findViewById(R.id.nav_view)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.menuItem1 -> Toast.makeText(applicationContext, "Clicked Item 1", Toast.LENGTH_LONG).show()
+                R.id.menuItem2 -> Toast.makeText(applicationContext, "Clicked Item 2", Toast.LENGTH_LONG).show()
+                R.id.menuItem3 -> Toast.makeText(applicationContext, "Clicked Item 3", Toast.LENGTH_LONG).show()
+            }
+            true
+        }
+
+//        val toolbar: Toolbar = findViewById(R.id.app_bar)
+//        setSupportActionBar(toolbar)
 
         val newGoal = findViewById<Button>(R.id.new_goal)
         newGoal.setOnClickListener {
@@ -33,6 +61,12 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (toggle.onOptionsItemSelected(item)) {
+            true
+        } else super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
