8cb8fca0bc6de4e0f3edf8aa61fe0850e60ee2d9
arvmarandi <arv.marandi@gmail.com>
2023-12-03 20:34:17 -0800

    Dynamically adds goals from SharedPreferences to the Navigation Drawer and dynamically adds functionality to each of these menu items.

--- app/src/main/java/com/example/triumphtree/MainActivity.kt
+++ app/src/main/java/com/example/triumphtree/MainActivity.kt
@@ -3,15 +3,20 @@ package com.example.triumphtree
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
+import androidx.core.view.iterator
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
+import com.google.gson.Gson
 
+
+const val MA1 = "MainActivity Name"
 class MainActivity : AppCompatActivity() {
     lateinit var drawerID: DrawerLayout
     lateinit var toggle: ActionBarDrawerToggle
@@ -31,17 +36,33 @@ class MainActivity : AppCompatActivity() {
 
         navView = findViewById(R.id.nav_view)
 
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.menuItem1 -> Toast.makeText(applicationContext, "Clicked Item 1", Toast.LENGTH_LONG).show()
-                R.id.menuItem2 -> Toast.makeText(applicationContext, "Clicked Item 2", Toast.LENGTH_LONG).show()
-                R.id.menuItem3 -> Toast.makeText(applicationContext, "Clicked Item 3", Toast.LENGTH_LONG).show()
-            }
-            true
+        val menu : Menu = navView.menu
+
+        val temp = readGoalsFromSharedPreferences()
+
+        for (i in temp) {
+            menu.add(i.name)
         }
 
-//        val toolbar: Toolbar = findViewById(R.id.app_bar)
-//        setSupportActionBar(toolbar)
+        Log.d(MA1, "test")
+
+        val items: MutableList<MenuItem> = mutableListOf()
+
+        for (i in menu.iterator()) {
+            items.add(i)
+            Log.d(MA1, "$i")
+        }
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            val position = items.indexOf(menuItem)
+            val selectedGoal = temp[position]
+
+            val intent = Intent(this@MainActivity, GoalDetails::class.java)
+            intent.putExtra("selectedGoal", selectedGoal)
+            startActivity(intent)
+
+            true
+        }
 
         val newGoal = findViewById<Button>(R.id.new_goal)
         newGoal.setOnClickListener {
@@ -61,6 +82,19 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+    }
+
+    private fun readGoalsFromSharedPreferences(): List<GoalModel>
+    {
+        val sharedPreferences =
+            getSharedPreferences("Goals", MODE_PRIVATE)
+
+        // Retrieve the JSON string from SharedPreferences
+        val jsonGoalsList = sharedPreferences.getString("GoalsList", null)
+
+        // Convert the JSON string back to a list of GoalModel using Gson
+        val type = object : TypeToken<List<GoalModel>>() {}.type
+        return Gson().fromJson(jsonGoalsList, type) ?: emptyList()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
